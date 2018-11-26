84862f6c5df01f4ce4d55d7de27985f0d1454749
HTML-Jeday <apprentice@i.ua>
2018-11-26 14:26:32 +0200

    some commit

---
+++ precache-manifest.92f06a57e3853221809c7f286bc07039.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6719ef350f951ba0ff6",
+    "url": "/static/css/main.b1d0b52f.chunk.css"
+  },
+  {
+    "revision": "f6719ef350f951ba0ff6",
+    "url": "/static/js/main.f6719ef3.chunk.js"
+  },
+  {
+    "revision": "da6646158cd815e3f971",
+    "url": "/static/js/1.da664615.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1d5b64ec5f8fe8089fd9370edd244ac4",
+    "url": "/static/media/manchester_city_logo.1d5b64ec.png"
+  },
+  {
+    "revision": "6de3706ade22bf1989b3853dda736986",
+    "url": "/static/media/featured_player.6de3706a.png"
+  },
+  {
+    "revision": "5432bcd5600bfba5ec63967d7efb05b9",
+    "url": "/static/media/Otamendi.5432bcd5.png"
+  },
+  {
+    "revision": "dd26db2771a362bbd922fa4686b5218a",
+    "url": "/static/media/jersey.dd26db27.jpg"
+  },
+  {
+    "revision": "43e9da416d9d49729b3a0ddf89e294a9",
+    "url": "/index.html"
+  }
+];
